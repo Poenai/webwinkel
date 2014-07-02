996e09ac2044b5d04abce6a58b6d2c4d4c208950
bakeds <bakeds@gmail.com>
2014-07-02 21:06:48 +0200

    fixed bug gaf niet altijd de juiste string terug alleen maar als de url aan bepaalde voorwaarde voldeed

--- js/cart/functions.js
+++ js/cart/functions.js
@@ -181,11 +181,40 @@ function addProductToCart(catalog, productId, quantity, property) {
 **/
 
 $.urlParam = function(name){
-    	var results = new RegExp('[\\?&amp;]' + name + '=([^&amp;#]*)').exec(window.location.href);
+    	/*var results = new RegExp('[\\?&amp;]' + name + '=([^&amp;#]*)').exec(window.location.href);
 	if(results==null) {
 		return "";
 	}
-    	return results[1] || 0;
+    	return results[1] || 0;*/
 
+    return getParmsFromURLHash(window.location.href)[name];
+
+}
+
+/**
+ * @bron = http://stackoverflow.com/questions/10624762/regex-to-extract-parameters-from-url-hash-in-javascript/10625052#10625052
+ * @param url
+ * @returns {{}}
+ */
+function getParmsFromURLHash(url) {
+    var parms = {}, pieces, parts, i;
+    var hash = url.lastIndexOf("#");
+    if (hash !== -1) {
+        // isolate just the hash value
+        url = url.slice(hash + 1);
+    }
+    var question = url.indexOf("?");
+    if (question !== -1) {
+        url = url.slice(question + 1);
+        pieces = url.split("&");
+        for (i = 0; i < pieces.length; i++) {
+            parts = pieces[i].split("=");
+            if (parts.length < 2) {
+                parts.push("");
+            }
+            parms[decodeURIComponent(parts[0])] = decodeURIComponent(parts[1]);
+        }
+    }
+    return parms;
 }
 
