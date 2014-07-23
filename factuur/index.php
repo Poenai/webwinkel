263f129542813a905b9fb1c080b7152417573753
bakeds <bakeds@gmail.com>
2014-07-23 20:29:29 +0200

    code om bezoekers toegevoegd aan de factuur

--- factuur/index.php
+++ factuur/index.php
@@ -21,6 +21,17 @@ $xml = simplexml_load_file("../xml/producten.xml");
         <script src="js/jquery.keyboard.min.js"></script>
         <script src="js/jquery.keyboard.extension-all.min.js"></script>
         <script src="js/notify.min.js"></script>
+        <!-- analytics om de bezoeker te volgen -->
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-35248865-4', 'auto');
+            ga('send', 'pageview');
+
+        </script>
 
     </head>
 	<body>
