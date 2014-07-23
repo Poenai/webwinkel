76cee2d200b9301694b67fc69a06721b84e86845
bakeds <bakeds@gmail.com>
2014-07-23 23:10:47 +0200

    Begonnen met codebase netter maken en voorbereiden voor uitbreidingen naar de toekomst door het deels OOP te maken hier de Producten Class deze class is static om dat je overal in de applicatie bij dezelfde gegevensbron wilt die afkomstig is van de producten.xml

---
+++ PHPLib/producten.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: barld
+ * Date: 23-7-14
+ * Time: 22:41
+ */
+class Producten {
+    //private fields
+    private static  $_fileName = "../xml/producten.xml";
+    private static $_products;
+
+    //public methods
+    public static function Initialize()
+    {
+        self::$_products = simplexml_load_file(self::$_fileName);
+    }
+
+    public static function GetAllProducts($JsonString = false)
+    {
+        if($JsonString)
+            return json_encode(self::$_products);
+        else
+            return self::$_products;
+    }
+
+    public static function GetProductByID($ID, $JsonString = false)
+    {
+        foreach(self::$_products->product as $product)
+        {
+            if($product->id == $ID)
+            {
+                if($JsonString)
+                    return json_encode($product);
+                else
+                    return $product;
+            }
+        }
+        throw new Exception("product not found");
+    }
+
+}
+
+//zorg dat er een aantal dingen zijn gebeurd in de static class voordat er voor de rest dingen mee gedaan worden
+Producten::Initialize();
