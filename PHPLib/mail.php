bf3277075bcbd450914976c82fb4f4d33afd7a98
barld <bakeds@gmail.com>
2014-07-25 22:54:21 +0200

    adresering toegevoegd aan mail class

--- PHPLib/mail.php
+++ PHPLib/mail.php
@@ -16,6 +16,26 @@ class Mail {
      */
     private $_body;
 
+    private $_templateLocation = "/MailTemplates/%s.template.php";
+    /**
+     * @var EmailContact[]
+     */
+    private $_to;
+    /**
+     * @var EmailContact[]
+     */
+    private $_bcc;
+
+    //getters en setters
+    /**
+     * @param string $body
+     */
+    public function setBody($body)
+    {
+        $this->_body = $body;
+    }
+
+
     public function __contsrtuct()
     {
 
@@ -24,10 +44,11 @@ class Mail {
     /**
      * @param string $name
      * @param array $variablen
+     * @throws Exception
      */
     public function SetTemplate($name, $variablen = array())
     {
-        if(file_exists(dirname(__FILE__)."/MailTemplates/".$name.".template.php"))
+        if(file_exists(dirname(__FILE__).sprintf($this->_templateLocation, $name)))
         {
             try{
                 //zorg dat de inhoud niet wordt weergegeven op het scherm
@@ -40,7 +61,7 @@ class Mail {
                 }
 
                 //laat de template uitvoeren
-                include dirname(__FILE__)."/MailTemplates/".$name.".template.php";
+                include dirname(__FILE__).sprintf($this->_templateLocation, $name);
 
 
                 $this->_body = ob_get_clean();
@@ -50,6 +71,33 @@ class Mail {
                 throw $e;
             }
         }
+        else
+        {
+            throw new Exception("template not found");
+        }
+    }
+
+    /**
+     * @param string $to
+     * @param string $naam
+     */
+    public function AddTo($to, $naam = null)
+    {
+        $this->_to[] = new EmailContact($to, $naam);
+    }
+
+    /**
+     * @param string $to
+     * @param string $naam
+     */
+    public function AddBcc($to, $naam = null)
+    {
+        $this->_bcc[] = new EmailContact($to, $naam);
+    }
+
+    public function Send()
+    {
+
     }
 }
 
