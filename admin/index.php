cacc6e4278a77cd56bc979448c11172cbcb38979
barld <bakeds@gmail.com>
2014-07-29 16:40:06 +0200

    ook voor rewrite

--- admin/index.php
+++ admin/index.php
@@ -11,6 +11,10 @@ require_once "adminController.php";
 if(isset($_GET['page']))
 {
     $c = new AdminController($_GET['page']);
+}else if(isset($_GET['q']))
+{
+    $_GET['page'] = chop($_GET['q'], '/');
+    $c = new AdminController(chop($_GET['q'], '/'));
 }
 else
     $c = new AdminController("facturen");
